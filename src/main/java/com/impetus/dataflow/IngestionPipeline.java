129e31d9cfc02909e1583ba6d86cd30d5153fd85
HarshMalviya2023 <123155805+HarshMalviya2023@users.noreply.github.com>
2023-02-28 10:15:45 +0530

    Update IngestionPipeline.java

--- src/main/java/com/impetus/dataflow/IngestionPipeline.java
+++ src/main/java/com/impetus/dataflow/IngestionPipeline.java
@@ -298,7 +298,7 @@ public class IngestionPipeline {
 
 		// get data from BQ1 as TableRow
 		PCollection<TableRow> bigQueryOutput = p.apply("Get-BigQuery-Data",
-				BigQueryIO.readTableRows().fromQuery(options.getQuery()).withoutValidation().usingStandardSql().withUseCachedResults(false));
+				BigQueryIO.readTableRows().fromQuery(options.getQuery()).withoutValidation().usingStandardSql().withMethod(BigQueryIO.TypedRead.Method.DIRECT_READ));
 
 		PCollectionView<List<List<String>>> fileHeaders = bigQueryOutput
 				.apply("Get-File-Headers", ParDo.of(new DoFn<TableRow, List<String>>() {
